33df742954923b501d5a7f8685546862b1d07521
Yuya Matsuo <niu@niusounds.com>
2020-02-03 00:58:32 +0900

    delete unused native codes

--- android/app/src/main/kotlin/com/niusounds/fl_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/niusounds/fl_player/MainActivity.kt
@@ -1,79 +1,12 @@
 package com.niusounds.fl_player
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import io.flutter.app.FlutterActivity
-import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity : FlutterActivity() {
-    companion object {
-        const val MY_PERMISSIONS_REQUEST = 12345
-    }
-
-    private var pendingGetAllArtistsResult: MethodChannel.Result? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         GeneratedPluginRegistrant.registerWith(this)
-
-        MethodChannel(flutterView, "fl_player/native_library").setMethodCallHandler { methodCall, result ->
-            when (methodCall.method) {
-                "getAllArtists" -> {
-
-                    if (ContextCompat.checkSelfPermission(
-                                    this,
-                                    Manifest.permission.READ_EXTERNAL_STORAGE
-                            ) != PackageManager.PERMISSION_GRANTED) {
-
-                        pendingGetAllArtistsResult = result
-                        ActivityCompat.requestPermissions(
-                                this,
-                                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                                MY_PERMISSIONS_REQUEST
-                        )
-
-                    } else {
-                        result.success(NativeLibrary.getAllArtists(this))
-                    }
-                }
-                "getAlbumsForArtist" -> {
-                    val artistName = methodCall.argument<String>("artistName")
-                    if (artistName == null) {
-                        result.error("invalid argument", "artistName is required", null)
-                    } else {
-                        result.success(NativeLibrary.getAlbumsForArtist(this, artistName))
-                    }
-                }
-                "getSongsForAlbum" -> {
-                    val albumId = methodCall.argument<Int>("albumId")
-                    if (albumId == null) {
-                        result.error("invalid argument", "albumId is required", null)
-                    } else {
-                        result.success(NativeLibrary.getSongsForAlbum(this, albumId))
-                    }
-                }
-                else -> {
-                    result.notImplemented()
-                }
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-            requestCode: Int,
-            permissions: Array<out String>,
-            grantResults: IntArray
-    ) {
-        when (requestCode) {
-            MY_PERMISSIONS_REQUEST -> {
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    pendingGetAllArtistsResult?.success(NativeLibrary.getAllArtists(this))
-                }
-            }
-        }
     }
 }
